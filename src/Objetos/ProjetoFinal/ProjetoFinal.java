4eec2e07800de22c31125cec5b7fca28270843d3
Tiago Correia <tiagocorreiabispo14@gmail.com>
2026-03-14 10:25:22 -0300

    Projeto Final (parte 2)
    Adicionada a classe que da relação entre vídeo e quem visualizou. Também foi reorganizado o código principal.

--- src/Objetos/ProjetoFinal/ProjetoFinal.java
+++ src/Objetos/ProjetoFinal/ProjetoFinal.java
@@ -13,10 +13,29 @@ public class ProjetoFinal {
         g[0] = new Gafanhoto("Pedrinho", 22, "M", "Jamelao");
         g[1] = new Gafanhoto("Luizinha", 32, "F", "Melao");
 
+        Visualizacao[] vis = new Visualizacao[5];
+        vis[0] = new Visualizacao(g[0], v[2]);
+        vis[1] = new Visualizacao(g[0], v[1]);
+        vis[2] = new Visualizacao(g[0], v[0]);
+        vis[3] = new Visualizacao(g[1], v[1]);
+        vis[4] = new Visualizacao(g[1], v[0]);
 
+        vis[0].avaliar();
+        System.out.println(vis[0].toString());
+        System.out.println();
 
+        vis[0].avaliar(87.0f);
+        System.out.println(vis[1].toString());
+
+        /*System.out.println("-----------VIDEOS----------");
         System.out.println(v[0].toString());
+        System.out.println(v[1].toString());
+        System.out.println(v[2].toString());
+        System.out.println("-----------GAFANHOTOS----------");
         System.out.println(g[0].toString());
+        System.out.println(g[1].toString());*/
+
+
 
     }
 }
